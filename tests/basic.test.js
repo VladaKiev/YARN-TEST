da4a427d9a7c334c414ae641a4a8e9e57263c766
vladakiev <zolvladislava@gmail.com>
2020-07-23 14:23:12 +0300

    the 1st one commit

---
+++ tests/basic.test.js
@@ -0,0 +1,11 @@
+//import {Selector} from "testcafe" :(
+    fixure ("Getting started with TestCafe")
+    .page ("https://devexpress.github.io/testcafe/example/")
+test("My first testcafe test", async t =>{
+    
+    await t.typeText("#developer-name", "VL")
+    await t.click("#submit-button")
+
+    await t.expect(Selector("#article-header").innerText).contains("VL")
+     //вернуться к .innerText= ??
+})
